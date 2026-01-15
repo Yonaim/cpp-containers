d0eb397bf102b8635be7b670fa91119ecef4b55c
Yonaim <yonaimyo@gmail.com>
2026-01-15 19:26:37 +0900

    Fix: Use iterator_tag of ft namespace when the type is pointer

--- include/ft/iterator/iterator_tags.h
+++ include/ft/iterator/iterator_tags.h
@@ -1,6 +1,14 @@
 #ifndef FT_ITERATOR_TAGS_H
 #define FT_ITERATOR_TAGS_H
 
+/*
+    iterator 카테고리 (아래로 갈수록 권한이 더 많음)
+    - input iterator: 한번만 읽기 가능, 단방향 (++it만 됨)
+    - forward iterator: 여러번 읽기 가능, 단방향 (++it만 됨)
+    - bidirectional iterator: 여러번 읽기 가능, 양방향(++it, --it 둘다 됨)
+    - random access iterator: 여러번 읽기 가능, 임의 접근 가능(it + n, it[n], it1 - it2 가능)
+*/
+
 namespace ft
 {
     struct input_iterator_tag
--- include/ft/iterator/iterator_traits.h
+++ include/ft/iterator/iterator_traits.h
@@ -4,6 +4,7 @@
 #include <cstddef>
 #include <iterator>
 #include "remove_cv.h"
+#include "iterator_tags.h"
 
 /*
 nested type을 그대로 갖고온다.
@@ -56,7 +57,7 @@ namespace ft
         typedef typename ft::remove_cv<T>::type value_type; // volatile 한정자 때문에 제거 필요
         typedef T                              *pointer;
         typedef T                              &reference;
-        typedef std::random_access_iterator_tag iterator_category;
+        typedef ft::random_access_iterator_tag iterator_category;
     };
 
     // specialization for const pointer
@@ -67,7 +68,7 @@ namespace ft
         typedef typename ft::remove_cv<T>::type value_type;
         typedef const T                        *pointer;
         typedef const T                        &reference;
-        typedef std::random_access_iterator_tag iterator_category;
+        typedef ft::random_access_iterator_tag iterator_category;
     };
 
 } // namespace ft
