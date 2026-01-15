da30c6dafaa8b2b9edbf1b90bd25ac99121bbb3b
Yonaim <yonaimyo@gmail.com>
2026-01-15 19:34:16 +0900

    Fix: umbrella headers

--- include/ft/algorithm/algorithm.h
+++ include/ft/algorithm/algorithm.h
@@ -1,6 +1,8 @@
 #ifndef FT_ALGORITHM_H
 #define FT_ALGORITHM_H
 
+#include "copy.h"
+#include "copy_backward.h"
 #include "equal.h"
 #include "lexicographical_compare.h"
 
--- include/ft/iterator/iterator.h
+++ include/ft/iterator/iterator.h
@@ -1,7 +1,9 @@
 #ifndef FT_ITERATOR_H
 #define FT_ITERATOR_H
+
 #include "distance.h"
 #include "iterator_tags.h"
 #include "iterator_traits.h"
 #include "reverse_iterator.h"
+
 #endif
